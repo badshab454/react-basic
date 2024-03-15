68ea33422aa5140782041edad9066b3e60611bea
Badsha Hossain <badshab454@gmail.com>
2024-03-16 03:29:38 +0600

    bootstrap installation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import './index.css';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
+function BasicExample() {
+  return (
+    <ButtonGroup aria-label="Basic example">
+      <Button variant="secondary">Left</Button>
+      <Button variant="secondary">Middle</Button>
+      <Button variant="secondary">Right</Button>
+    </ButtonGroup>
+  );
+}
 
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello React</h2>
+      <h2 style={{ color: "red", backgroundColor: "Green" }}>Hello React</h2>
+      <BasicExample />
     </div>
   );
 }
